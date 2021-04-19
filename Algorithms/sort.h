518ad0e70badab2f7f3a5d48ee90e98adf8033a9
Kabir Khara <kabirkhara@gmail.com>
2021-04-19 14:44:07 -0400

    can now sort by price and point value

--- Algorithms/sort.h
+++ Algorithms/sort.h
@@ -8,7 +8,7 @@ using namespace std;
 //Compare algorithm time complexities to complete similar tasks
 
 //BUBBLE SORT, psuedocode cited from slides module 6
-void bubbleSort(vector<Wine>& vect) 
+void bubbleSortPrice(vector<Wine>& vect) 
 {
   for (int i = 0; i < vect.size() - 1; i++) 
   {
@@ -30,8 +30,34 @@ void bubbleSort(vector<Wine>& vect)
   }
 }
 
+void bubbleSortPoint(vector<Wine>& vect) 
+{
+  for (int i = 0; i < vect.size() - 1; i++) 
+  {
+    int swapped = 0;
+    for (int j = 0; j < vect.size() - i - 1; ++j) 
+    {
+      if (vect[j].getPoints() > vect[j + 1].getPoints()) 
+      {
+        Wine temp = vect[j];
+        vect[j] = vect[j + 1];
+        vect[j + 1] = temp;
+        swapped = 1;
+      }
+    }
+    if (swapped == 0)
+    {
+        break;
+    }
+  }
+}
+
+
+
+
+
 //MERGE SORT, psuedocode cited from slides module 6
-void merge(vector<Wine>& vect, int left, int mid, int right) 
+void mergePrice(vector<Wine>& vect, int left, int mid, int right) 
 {
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -82,21 +108,89 @@ void merge(vector<Wine>& vect, int left, int mid, int right)
   }
 }
 
-
-void mergeSort(vector<Wine>& vect, int left, int right) 
+void mergeSortPrice(vector<Wine>& vect, int left, int right) 
 {
   if (left < right) 
   {
     // m is the point where the array is divided into two subarrays
     int mid = left + (right - left) / 2;
 
-    mergeSort(vect, left, mid);
-    mergeSort(vect, mid + 1, right);
+    mergeSortPrice(vect, left, mid);
+    mergeSortPrice(vect, mid + 1, right);
 
     // Merge the sorted subarrays
-    merge(vect, left, mid, right);
+    mergePrice(vect, left, mid, right);
   }
 }
+
+void mergePoints(vector<Wine>& vect, int left, int mid, int right) 
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<Wine> X;
+    vector<Wine> Y;
+
+    for (int i = 0; i < n1; i++)
+    {
+        X[i] = vect[left + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        Y[j] = vect[mid + 1 + j];
+    }
+
+  // Merge the arrays X and Y into vector
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = left;
+  while (i < n1 && j < n2) 
+  {
+    if (X[i].getPoints() <= Y[j].getPoints()) 
+    {
+      vect[k] = X[i];
+      i++;
+    } 
+    else 
+    {
+      vect[k] = Y[j];
+      j++;
+    }
+    k++;
+  }
+ 
+  while (i < n1) 
+  {
+    vect[k] = X[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) 
+  {
+    vect[k] = Y[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSortPoints(vector<Wine>& vect, int left, int right) 
+{
+  if (left < right) 
+  {
+    int mid = left + (right - left) / 2;
+
+    mergeSortPoints(vect, left, mid);
+    mergeSortPoints(vect, mid + 1, right);
+
+    // Merge the sorted subarrays
+    mergePoints(vect, left, mid, right);
+  }
+}
+
+
+
+
 
 //QUICK SORT, psuedocode cited from slides module 6
 void swap(Wine *a, Wine *b)
@@ -106,7 +200,7 @@ void swap(Wine *a, Wine *b)
     *b = t;
 }
 
-int partition(vector<Wine>& vect, int low, int high) 
+int partitionPrice(vector<Wine>& vect, int low, int high) 
 {
   int pivot = vect[low].getPrice();
   int up=low, down=high;
@@ -138,12 +232,54 @@ int partition(vector<Wine>& vect, int low, int high)
   return down;
 }
 
-void quickSort(vector<Wine>& vect, int low, int high) 
+void quickSortPrice(vector<Wine>& vect, int low, int high) 
 {
   if (low < high) 
   {
-    int pivot = partition(vect, low, high);
-    quickSort(vect, low, pivot - 1);
-    quickSort(vect, pivot + 1, high);
+    int pivot = partitionPrice(vect, low, high);
+    quickSortPrice(vect, low, pivot - 1);
+    quickSortPrice(vect, pivot + 1, high);
+  }
+}
+
+int partitionPoints(vector<Wine>& vect, int low, int high) 
+{
+  int pivot = vect[low].getPoints();
+  int up=low, down=high;
+  
+  while(up<down)
+  {
+      for (int j = up; j < high; j++)
+      {
+        if(vect[up].getPoints() > pivot)
+        {
+            break;
+        }
+        up++;
+      }
+      for (int j = high; j > low; j--)
+      {
+        if(vect[down].getPoints() < pivot)
+        {
+            break;
+        }
+        down--;
+      }
+      if(up<down)
+      {
+        swap(&vect[up], &vect[down]);
+      }
+  }
+  swap(&vect[low], &vect[down]);
+  return down;
+}
+
+void quickSortPoints(vector<Wine>& vect, int low, int high) 
+{
+  if (low < high) 
+  {
+    int pivot = partitionPoints(vect, low, high);
+    quickSortPoints(vect, low, pivot - 1);
+    quickSortPoints(vect, pivot + 1, high);
   }
 }
