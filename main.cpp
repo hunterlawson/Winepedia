02438b0c57ac7c73859ee7df3701903db59a4f72
Hunter Lawson <hunter.lawson@ufl.edu>
2021-04-19 06:23:25 -0400

    changed comments

--- main.cpp
+++ main.cpp
@@ -15,11 +15,11 @@
 using namespace std;
 
 // Selects Wines from the database where attribute # 'columnNum' matches 'term'
-// Returns the result into the passed-in vector of wines
-void selectData(const char* dir, int column, string match, vector<Wine>* result);
+// Returns the result into the passed-in pointer to a vector of wines
+void selectData(const char* dir, int column, string match, vector<Wine>* container);
 // Adopted from the SQLite documentation
 // Retrieves data from the call in selectData() and passes the wines into the container
-int callback(void* notused, int argc, char** argv, char** azColName);
+int callback(void* container, int argc, char** argv, char** azColName);
 
 int main() {
     const char* dataDir = "Data/wineData.db";
@@ -32,7 +32,7 @@ int main() {
     return 0;
 }
 
-void selectData(const char* dir, int columnNum, string term, vector<Wine>* result) {
+void selectData(const char* dir, int columnNum, string term, vector<Wine>* container) {
     sqlite3* database;
     sqlite3_open(dir, &database); // Open the database
 
@@ -46,15 +46,15 @@ void selectData(const char* dir, int columnNum, string term, vector<Wine>* resul
         case 5: columnName = "Winery"; break;
     }
     string query = "SELECT * FROM Wines WHERE " + columnName + " = \'" + term + "\';";
-    sqlite3_exec(database, query.c_str(), callback, (void*) result, nullptr);
-    sqlite3_close(database);
+    sqlite3_exec(database, query.c_str(), callback, (void*) container, nullptr);
+    sqlite3_close(database); // Close the database
 }
 
 int callback(void* container, int argc, char** arg, char** azColName) {
     vector<Wine>* tempWines = (vector<Wine>*) container;
 
     string checkNull = arg[4];
-    if(checkNull.size() == 0) return 0; // Return if the price is null
+    if(checkNull.size() == 0) return 0; // Return 0 if the price is null
 
     int num = stoi(arg[0]);
     string country = arg[1];
